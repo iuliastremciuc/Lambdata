504e7dca0ab64d6a84d6d360778056ea73981b39
Iulia Stremciuc <iuliastremciuc@gmail.com>
2020-03-09 20:40:29 -0500

    updates

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -3,3 +3,20 @@
 def enlarge(x):
 
     return x * 100
+    
+ #this code breaks our ability to import enlarged from other files
+   
+
+
+# print("Hello")
+# y = int(input("Please choose a number"))
+# print(y, enlarge(y))
+
+if __name__ == "__main__":
+  #  only run the code below If this script invoked from the command
+  # not if it imported from another
+    print("Hello")
+    y = int(input("Please choose a number"))
+    print(y, enlarge(y))
+    
+
